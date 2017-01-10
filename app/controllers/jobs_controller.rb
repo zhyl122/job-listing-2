2c34614d5982bc7ec2a97c076a2b93d3ab30af96
zhouyilin <zhyl9697@gmail.com>
2017-01-10 20:51:17 +0800

    implement part 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,21 +1,5 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  def show
-    @job = Job.find(params[:id])
-  end
-
-  def edit
-      @job = Job.find(params[:id])
-    end
-
-    def update
-      @job = Job.find(params[:id])
-      if @job.update(job_params)
-        redirect_to jobs_path
-      else
-        render :edit
-      end
-    end
 
   def index
     @jobs = Job.all
@@ -24,6 +8,24 @@ class JobsController < ApplicationController
   def new
       @job = Job.new
     end
+
+    def show
+      @job = Job.find(params[:id])
+    end
+
+    def edit
+        @job = Job.find(params[:id])
+      end
+
+      def update
+        @job = Job.find(params[:id])
+        if @job.update(job_params)
+          redirect_to jobs_path
+        else
+          render :edit
+        end
+      end
+
 
     def create
       @job = Job.new(job_params)
